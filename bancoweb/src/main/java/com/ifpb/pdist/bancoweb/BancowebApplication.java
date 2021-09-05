d6b954765317abcaaf0b456327277a21db141c15
andreia23 <andreialima.berto08@gmail.com>
2021-09-04 22:54:35 -0300

    producer-api

---
+++ bancoweb/src/main/java/com/ifpb/pdist/bancoweb/BancowebApplication.java
@@ -0,0 +1,13 @@
+package com.ifpb.pdist.bancoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancowebApplication.class, args);
+	}
+
+}
---
+++ bancoweb/src/test/java/com/ifpb/pdist/bancoweb/BancowebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpb.pdist.bancoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
