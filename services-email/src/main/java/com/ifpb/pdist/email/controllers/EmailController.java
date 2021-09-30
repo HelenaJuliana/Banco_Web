435ebfc0cbb4e76eec440373a38904006fc04cfa
andreia23 <andreialima.berto08@gmail.com>
2021-09-30 12:14:11 -0300

    enviando email

--- services-email/src/main/java/com/ifpb/pdist/email/controllers/EmailController.java
+++ services-email/src/main/java/com/ifpb/pdist/email/controllers/EmailController.java
@@ -1,9 +1,11 @@
 package com.ifpb.pdist.email.controllers;
 
 
-import com.ifpb.pdist.email.dtos.EmailDto;
-import com.ifpb.pdist.email.models.EmailModel;
-import com.ifpb.pdist.email.services.EmailService;
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,12 +13,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.util.Optional;
-import java.util.UUID;
+import com.ifpb.pdist.email.dtos.EmailDto;
+import com.ifpb.pdist.email.models.EmailModel;
+import com.ifpb.pdist.email.services.EmailService;
 
 @RestController
 public class EmailController {
@@ -24,9 +32,14 @@ public class EmailController {
     @Autowired
     EmailService emailService;
 
-    @PostMapping("/sending-email")
+    
+	@RequestMapping(value = "/sending-email", method = RequestMethod.POST, produces = { MediaType.APPLICATION_JSON_VALUE }, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto) {
         EmailModel emailModel = new EmailModel();
+        emailDto.setOwnerRef("Helena");
+        emailDto.setEmailFrom("helenabarros245@gmail.com");
+        emailDto.setSubject("Banco Web");
+        emailDto.setText("Bem vindo ao banco Web plataforma que faz transações rápidas e seguras! *-*");
         BeanUtils.copyProperties(emailDto, emailModel);
         emailService.sendEmail(emailModel);
         return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
--- services-email/src/main/java/com/ifpb/pdist/email/dtos/EmailDto.java
+++ services-email/src/main/java/com/ifpb/pdist/email/dtos/EmailDto.java
@@ -1,24 +1,75 @@
 package com.ifpb.pdist.email.dtos;
 
-import lombok.Data;
+import java.io.Serializable;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
-@Data
-public class EmailDto {
 
-    @NotBlank
+public class EmailDto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+    @Email
+    @NotNull
+    private String emailTo;
+    
+
     private String ownerRef;
-    @NotBlank
+
     @Email
     private String emailFrom;
-    @NotBlank
-    @Email
-    private String emailTo;
-    @NotBlank
-    private String subject;
-    @NotBlank
-    private String text;
 
+    private String subject;
+
+    
+    private String text;
+    
+
+	public String getEmailTo() {
+		return emailTo;
+	}
+
+	public void setEmailTo(String emailTo) {
+		this.emailTo = emailTo;
+	}
+
+	public String getOwnerRef() {
+		return ownerRef;
+	}
+
+	public void setOwnerRef(String ownerRef) {
+		this.ownerRef = ownerRef;
+	}
+
+	public String getEmailFrom() {
+		return emailFrom;
+	}
+
+	public void setEmailFrom(String emailFrom) {
+		this.emailFrom = emailFrom;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+
+   
 }
